c9d0b66709344dd137970be4a81950113485e9ec
Tinkidinki <mahathi98@gmail.com>
2019-11-30 21:19:21 +0530

    added non-optimal witness, wrote performance check function

--- analytical_witness.m
+++ analytical_witness.m
@@ -1,9 +1,12 @@
 function funs = analytical_witness
     funs.cvenn_werner_like = @cvenn_werner_like
     funs.state_on_boundary = @state_on_boundary;
-    funs.grad = @grad;
+    % funs.grad = @grad;
     funs.witness = @witness;
     funs.werner_like_state = @werner_like_state;
+    funs.test = @testfun;
+    funs.non_optimal_witness = @non_optimal_witness;
+    funs.witness_performance_on_werner = @witness_performance_on_werner;
 end
 
 % function g = grad(sigma)
@@ -26,19 +29,19 @@ function c = cvenn_werner_like(p, rho, d)
     c = trace(sigma*logm(sigma)) - trace(sigma_b*logm(sigma_b))
 end
 
-function g = grad(sigma, d)
+function w = witness(sigma, d)
     % need to take care of log 0.
     I = eye(d)
     sigma_b = TrX(sigma, 1, [d,d]);
-    g = transpose(logm(sigma) - kron(I, logm(sigma_b)));
+    g = -logm(sigma) + kron(I, logm(sigma_b));
 end
     
-function w = witness(rho, d)
-    sigma = state_on_boundary(rho, d)
-    g = grad(sigma, d);
-    I = eye(d*d);
-    w = (trace(sigma*g)*I - g)/(sqrt(trace(g)^2));
-end
+% function w = witness(rho, d)
+%     sigma = state_on_boundary(rho, d)
+%     g = grad(sigma, d);
+%     I = eye(d*d);
+%     w = (trace(sigma*g)*I - g)/(sqrt(trace(g)^2));
+% end
 
 function sigma = state_on_boundary(rho, d)
     p = fsolve(@(p)cvenn_werner_like(p, rho, d), 0.1)
@@ -56,3 +59,42 @@ function rho = werner_like_state(p,d)
     phi_plus_matrix = phi_plus_matrix/d
     rho = p*phi_plus_matrix + (1-p)*eye(d^2)/d^2
 end
+
+% No, need to change, has been written correctly.
+function w = non_optimal_witness(rho_s, d)
+    I = eye(d)
+    rho_sb = TrX(rho_s, 1, [d,d]);
+    w = -logm(rho_s) + kron(I, logm(rho_sb));
+end
+
+
+% function ans = testfun(d)
+%     ans = 0
+%     for i = 1:100
+%         rho = randRho(d*d)
+%         if quantum_cond_entr(rho, [d d]) < 0:
+%             disp(i);
+%             w = witness(rho, d)
+%             if check_witness(w, rho) >= 0:
+%                 disp("uh oh")
+%                 ans = -1
+
+function wp = witness_performance_on_werner(w, d):
+    prec = 100; % set precision, the higher this number is, the better - will take longer to compute though.
+    p = zeros(1, prec);
+    cond_entr = zeros(1, prec);  %The quantum conditional entropy
+    wit_res = zeros(1, prec);  %The result of the witness
+    for i = 1:100
+        p(i) = i/100;
+        rho = werner_like_state(p(i), d);
+        cond_entr(i) = quantum_cond_entr(rho, [d d]);
+        wit_res(i) = check_witness(w, rho);
+    end
+    
+    plot(p, cond_entr);
+    hold on;
+    plot(p, wit_res);
+    wp = 0;
+
+end
+
