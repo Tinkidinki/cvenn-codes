5238a696026822f75509545f48e519128ef1d8fe
Tinkidinki <mahathi98@gmail.com>
2021-01-08 17:43:36 +0530

    The upper bound works quite well

--- tight_upper_bound.m
+++ tight_upper_bound.m
@@ -5,12 +5,16 @@ n = 4;
 cvx_begin
     variable rho(4,4) hermitian semidefinite
     maximize quantum_cond_entr(rho, [2 2]) / log(2)
-    t = class(sigma)
     
     subject to 
         trace(rho) == 1
         % (- trace_logm(sigma, rho) + trace_logm(TrX(sigma, [1], [2,2]), TrX(rho, [1], [2,2]))) / log(2) == chi
-        - trace_logm(cvx_constant(sigma), rho) / log(2) == chi
+        % - trace_logm(sigma, rho) / log(2) == chi
+        % trace_logm(rho, sigma) / log(2) >= 0.2
+        % - trace(rho * logm(sigma)/log(2)) >=0.1 
+        
+        -trace(rho * logm(sigma)/log(2)) + trace(TrX(rho, [1], [2,2]) * logm(TrX(sigma, [1], [2,2]))/log(2)) == chi
+
 cvx_end
 
 
